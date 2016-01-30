cd9305b87fd22dcdf90055132a951846152c38fe
FatConan <ian@headwillcollapse.net>
2016-01-30 04:45:20 +0000

    Addign some junky experimnetation using a sprite sheet, quads and a sprite batch for attempting to render arenas

--- conf.lua
+++ conf.lua
@@ -2,8 +2,8 @@
 function love.conf(t)
     t.title = "Pigeon Posse"
     t.version = "0.10.0"
-    -- t.window.width = 800
-    -- t.window.height = 600
+    t.window.width = 1024
+    t.window.height = 896
 
     -- For Windows debugging
     t.console = true

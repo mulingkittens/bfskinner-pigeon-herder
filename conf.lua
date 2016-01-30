abe7883009ac8f9cb461542f201aba369f0e3124
Andy Durdin <me@andy.durdin.net>
2016-01-30 00:54:19 +0000

    Files to get started

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+    t.title = "Pigeon Posse"
+    t.version = "0.10.0"
+    -- t.window.width = 800
+    -- t.window.height = 600
+
+    -- For Windows debugging
+    t.console = true
+end

f4560820dee79e3d71c152ce4b79570c9745e455
Gordon <gordon@codeplay.com>
2016-01-30 14:02:13 +0000

    Move variables to pigeon variables file

--- src/pigeon.lua
+++ src/pigeon.lua
@@ -1,15 +1,4 @@
 require("src/util")
-pigeonSpeed = 50;
-
-foodMaximum = 100
-fooPerFeed = 10
-foodDecrement = 0.1
-
-pigeonInfluencePerClick = 20
-pigeonInfluenceMax = 100
-pigeonInfluenceUpperThreshold = 75
-pigeonInfluenceLowerThreshold = 25
-pigeonInfluenceDecrement = 1
 
 state = {
     alive = 0,

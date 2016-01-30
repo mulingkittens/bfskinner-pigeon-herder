cd9ad7e760c66470a132a489451539fb6f7317ec
Luke Drummond <lukedrummond@gmail.com>
2016-01-30 13:39:43 +0000

    fix the level string parser

--- src/loader.lua
+++ src/loader.lua
@@ -8,14 +8,15 @@
    A map is a simple ASCII grid, layed out as follows
 
    ^-------------
-   |#############
-   |#############
-   |######S######
-   |###B#########
-   |#P###########
+   |             
+   |             
+   |      S      
+   |   B         
+   |P            
    --------------
 
-For each square on the grid in the map, The given ASCII character is looked up in the engine's `map_items` module, and indexed for the given character.
+For each square on the grid in the map, The given ASCII character is looked up in the engine's `map_items` module,
+and indexed for the given character.
 If the given character's key exists in the table its value (any callable) is called with the tuple (x, y, existing_constructors).
 The called function is expected to return a new Game object which is the placed on a new grid at the same coordinate
 
@@ -30,34 +31,42 @@ The following items are predefined and can be overriden:
 
 local function parse_map(map_s)
     -- given a map string `map_s` parse as a multidimensional array
-    -- with the origin (top-left) as the single caret character
-    -- leading whitespace is stripped
-    local rows = {}
-    local columns = {}
-    for line in map_s:find("([^%s])") do
-        rows[#rows + 1] = {}
-        for char in line:gmatch("%s+(.+)") do
-            rows[#rows + 1] = char
+    -- with the origin at the top-left.
+    local lines = {}
+    local grid = {}
+    nonempty = false
+    map_s:gsub('(.-)\r?\n', function(line)
+        if nonempty or line:find("[^%S]") then
+            nonempty = true
+            lines[#lines + 1] = line
+        end
+    end)
+    for i, line in ipairs(lines) do
+        local t = {}
+        grid[i] = t
+        for char in line:gmatch("(.)") do
+            t[#t + 1] = char
         end
     end
+    return grid
 end
 
 
-local function load_level_file(level_name)
+local function load_level_file(level_name, fs)
     -- load a level file and return a level object which contains the raw
     -- lua chunk
-    local fs = love.filesystem
+    local fs = fs or love and love.filesystem or nil
     local predefined = predefined or {}
     local success, chunk = pcall(fs.load, level_name)
 
     if not success then return success, chunk end
 
-    return pcall(chunk(game_ctx))
+    return pcall(chunk)
 end
 
 
 local function construct_level(level_cfg, map_grid, default_constructors)
-    level_cfg.constructors = level_cfg.map_items or function() end
+    level_cfg.constructors = Game.objects.default_constructors or function() end
     local level = {}
     for x, row in ipairs(map_grid) do
         level[x] = {}

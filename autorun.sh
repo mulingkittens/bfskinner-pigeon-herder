90ca76b42809724f6953429b2b23dd3d3c54a18e
Andy Durdin <me@andy.durdin.net>
2016-01-30 07:39:25 +0000

    Add autorun script for demo machine

---
+++ autorun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+current_hash=
+while true; do
+    new_hash=$(git ls-remote origin master)
+    if [[ "$new_hash" != "$current_hash" ]]; then
+        current_hash="$new_hash"
+        killall love
+        ./build.sh &
+        disown
+        sleep 10
+    fi
+done

3e21e8113267a450e2ffed1ecf9cbbeb98510d27
FatConan <ian@headwillcollapse.net>
2016-01-30 18:58:44 +0000

    More stuffs

---
+++ src/sprite_loader/sprite_manager.lua
@@ -0,0 +1,10 @@
+return function(spriteSheetFile)
+    local spriteSheet = love.graphics.newImage(spriteSheetFile)   
+    local mtable = {
+        sprites = {},
+        spriteSheet = spriteSheet,
+        spriteBatch = nil,
+    }
+    
+    return mtable
+    end

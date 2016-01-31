f19752dd747b141a19e0fd3677e88a9850106293
Luke Drummond <lukedrummond@gmail.com>
2016-01-31 08:34:18 +0000

    Give it the title it deserves

--- conf.lua
+++ conf.lua
@@ -1,13 +1,12 @@
 -- Configuration
 function love.conf(t)
-    t.title = "Pigeon Posse"
+    t.title = [[B.F. Skinner: Pigeon Herder (LEGITIMATE PIGEON SIMULATOR)]]
     t.version = "0.10.0"
     -- This is just the initial window size; it is readjusted in main.lua
     t.window = {
         width = 640,
         height = 480,
    }
-
     -- For Windows debugging
     t.console = true
 end
